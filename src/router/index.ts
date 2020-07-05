26bf09f8102bafecc224823fe8c977ecae64df72
chenzhen <373428582@qq.com>
2020-07-05 22:34:16 +0800

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import { component } from 'vue/types/umd';
+
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path:'/login',
+    name:'login',
+    component:Login
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login-wrap">
+    <h1>登录1</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  components: {}
+};
+</script>
