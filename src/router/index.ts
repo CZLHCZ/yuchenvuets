259e0f92bd6836d31e0135a3ff0426e0936a7f17
chenzhen <373428582@qq.com>
2020-07-06 14:38:11 +0800

    添加首页，路由界面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../views/home/home.vue";
 import Login from "../views/Login.vue";
 import { component } from 'vue/types/umd';
 
---
+++ src/views/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home-wrpa">
+    <h2>首页</h2>
+  </div>
+</template>
+
+<script>
+const props = {};
+const data = () => ({});
+const computed = {};
+const watch = {};
+const methods = {};
+const components = {};
+export default {
+  name: "",
+  props,
+  data,
+  computed,
+  watch,
+  methods,
+  mounted() {
+    console.log("首页");
+  },
+  components
+};
+</script>
+
+<style scoped lang="less"></style>
